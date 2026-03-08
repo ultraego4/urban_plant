0efdd7a670d9240d315083e5d46ca1fabcb4e585
UltraEgo4 <ujfalus.biz@gmail.com>
2026-03-08 16:17:03 +0100

    refactoring, removing old code

--- main/app_main.c
+++ main/app_main.c
@@ -1,20 +1,3 @@
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
-#include "esp_log.h"
-#include "i2cdev.h"
-
-#include "sensors/veml7700_sensor.h"
-#include "sensors/bmp280_sensor.h"
-
 void app_main(void) {
-    ESP_ERROR_CHECK(i2cdev_init());
 
-    static veml7700_wrapper_t veml_sensor = {0};
-    static bmp280_wrapper_t bmp_sensor = {0};
-
-    if(veml7700_setup(&veml_sensor) != ESP_OK) return;
-    if(bmp280_setup(&bmp_sensor) != ESP_OK) return;
-
-    xTaskCreate(veml7700_task, "veml_task", 4096, &veml_sensor, 5, NULL);
-    xTaskCreate(bmp280_task, "bmp_task", 8192, &bmp_sensor, 5, NULL);
 }
